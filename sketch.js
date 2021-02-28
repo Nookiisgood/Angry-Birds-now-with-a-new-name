4fdd151d302ae5e0a00cd0b53239e76200482d4b
Nikki Cortes <satanicoa145@gmail.com>
2021-02-28 18:28:21 -0500

    added a new pig

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1, pig2;
 var backgroundImg,platform;
 
 function preload() {
@@ -23,6 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    pig2 = new Pig(800, 200);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -54,6 +55,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    pig2.display();
 
     box5.display();
     log4.display();
